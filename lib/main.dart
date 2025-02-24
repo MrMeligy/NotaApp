a4a2cbd63de565f632a0a2f89423155b6d486376
MrMeligy <youssefelzyat72@gmail.com>
2025-02-24 15:29:47 +0200

    add cubit to modal sheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:nota/consts.dart';
+import 'package:nota/cubits/cubit/add_nota_cubit.dart';
 import 'package:nota/models/nota_model.dart';
 import 'package:nota/views/home_view.dart';
 
@@ -17,10 +19,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
-      home: HomeView(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => AddNotaCubit()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark(),
+        home: HomeView(),
+      ),
     );
   }
 }
--- lib/widgets/add_note_modal_sheet.dart
+++ lib/widgets/add_note_modal_sheet.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:nota/cubits/cubit/add_nota_cubit.dart';
 import 'package:nota/widgets/nota_form.dart';
 
 class AddNoteModalSheet extends StatelessWidget {
@@ -12,7 +15,23 @@ class AddNoteModalSheet extends StatelessWidget {
       ),
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 12),
-        child: NotaForm(),
+        child: BlocConsumer<AddNotaCubit, AddNotaState>(
+          listener: (context, state) {
+            if (state is AddNotaSuccess) {
+              Navigator.pop(context);
+            }
+            if (state is AddNotaFailure) {
+              SnackBar(
+                  content: Text('Failed to add note, ${state.errorMessage}'));
+            }
+          },
+          builder: (context, state) {
+            return ModalProgressHUD(
+              inAsyncCall: state is AddNotaLoading ? true : false,
+              child: const NotaForm(),
+            );
+          },
+        ),
       ),
     );
   }
