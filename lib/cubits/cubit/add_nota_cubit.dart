44056caa25fb5685efcc50bbf475094174c9e95b
MrMeligy <youssefelzyat72@gmail.com>
2025-02-24 14:30:21 +0200

    Add Add nota cubit

--- lib/cubits/cubit/add_nota_cubit.dart
+++ lib/cubits/cubit/add_nota_cubit.dart
@@ -1,8 +1,21 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
+import 'package:nota/consts.dart';
+import 'package:nota/models/nota_model.dart';
 
 part 'add_nota_state.dart';
 
 class AddNotaCubit extends Cubit<AddNotaState> {
   AddNotaCubit() : super(AddNotaInitial());
+  addNota(NotaModel nota) async {
+    emit(AddNotaLoading());
+    try {
+      var notesBox = Hive.box<NotaModel>(kBoxName);
+      await notesBox.add(nota);
+      emit(AddNotaSuccess());
+    } catch (e) {
+      emit(AddNotaFailure(e.toString()));
+    }
+  }
 }
