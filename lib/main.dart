733e74f2bbeddf0785a018f9966b5c67f23f0be9
MrMeligy <youssefelzyat72@gmail.com>
2025-02-24 12:47:09 +0200

    Init Hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:nota/consts.dart';
 import 'package:nota/views/home_view.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox(kBoxName);
   runApp(const MyApp());
 }
 
